06b2fdeb56389cba5f9c8dcdb5ce08dd03b9e919
이종상 <helius96@gmail.com>
2021-04-09 10:36:26 +0900

    my gugudan edited

--- my_gugudan.py
+++ my_gugudan.py
@@ -1,6 +1,6 @@
 print("★ 구구단을 출력합니다.\n")
-for x in range(2, 9):
+for x in range(2, 10):
     print("------- [" + str(x) + "단] -------")
-    for y in range(1, 9):
+    for y in range(1, 10):
         print(x, "X", y, "=", x*y)
 print("---------------------")
